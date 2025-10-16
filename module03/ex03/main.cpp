a6fdb889737a74b730da42e08aface9b3949f8f4
Kacper <kacper.orzechowski.04@gmail.com>
2025-10-16 16:49:31 +0200

    module 3 simple main

--- module03/ex03/main.cpp
+++ module03/ex03/main.cpp
@@ -2,24 +2,8 @@
 
 int main ()
 {
-	ScavTrap scavTrap("Klapek");
-	ClapTrap clapTrap("Ballerina");
-	FragTrap fragTrap("Jan Pawel II");
 	DiamondTrap diamondTrap("Kilof");
 
-	clapTrap.attack("Dupa!");
-	clapTrap.beRepaired(15);	
-	clapTrap.takeDamage(1);
-	std::cout << "--------------------\n";
-	scavTrap.attack("Sahur");
-	scavTrap.beRepaired(15);	
-	scavTrap.guardGate();
-	scavTrap.takeDamage(30);
-	std::cout << "--------------------\n";
-	fragTrap.attack("Niewierni");
-	fragTrap.takeDamage(44);
-	fragTrap.beRepaired(100);
-	fragTrap.highFivesGuys();
 	std::cout << "--------------------\n";
 	diamondTrap.attack("Wengiel");
 	diamondTrap.whoAmI();
@@ -27,4 +11,5 @@ int main ()
 	diamondTrap.guardGate();
 	diamondTrap.highFivesGuys();
 	diamondTrap.takeDamage(100);
+	std::cout << "--------------------\n";
 }
