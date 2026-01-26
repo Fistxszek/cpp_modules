4385949957d33c341f4646d7e556098638c22025
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-01-26 16:37:59 +0100

    argv

--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -1,7 +1,10 @@
 #include "ScalarConverter.hpp"
+#include <cmath>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::string abc = "   43f";
+	if (argc != 2)
+		return 1;
+	std::string abc = argv[1];
 	ScalarConverter::convert(abc);
 }
