62adea85b362822110bab5dd9fefc19c8f727473
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-01-28 18:13:03 +0100

    cpp06/ex02 fin

---
+++ module06/ex02/Base.hpp
@@ -0,0 +1,16 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <stdint.h>
+
+class A;
+class B;
+class C;
+
+class Base
+{
+	public:
+		virtual ~Base();
+};
+
+#endif
--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -1,5 +1,83 @@
+#include <cstdlib>
 #include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *Generate(void)
+{
+	srand(time(0));
+	int r = rand() % 3;
+	switch (r)
+	{
+		case 0:
+			return (new A());
+		case 1:
+			return (new B());
+		case 2:
+			return (new C());
+	}
+	return NULL;
+}
+
+void Identify(Base *p)
+{
+	std::stringstream ss("");
+
+	if (dynamic_cast<A*>(p))
+		ss << "A";
+	if (dynamic_cast<B*>(p))
+		ss << "B";
+	if (dynamic_cast<C*>(p))
+		ss << "C";
+
+	std::string str;
+	ss >> str;
+	std::cout << "Actual type of the *p is : " << str << std::endl;
+}
+
+void Identify(Base &p)
+{
+	std::stringstream ss("");
+	std::string str;
+	
+	try
+	{
+		dynamic_cast<A&>(p);
+		ss << "A";
+	}
+	catch (const std::bad_cast &e){  }
+
+	try
+	{
+		dynamic_cast<B&>(p);
+		ss << "B";
+	}
+	catch (const std::bad_cast &e){  }
+
+	try
+	{
+		dynamic_cast<C&>(p);
+		ss << "C";
+	}
+	catch (const std::bad_cast &e){  }
+
+	ss >> str;
+	std::cout << "Actual type of the &p is : " << str << std::endl;
+}
 
 int main()
 {
+	Base* b;
+	std::cout << "====Type====\n";
+	b = Generate();
+	Identify(*b);
+	Identify(b);
+
+	delete b;
 }
