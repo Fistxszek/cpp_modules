1f7119c63ef30516923a3164b40acdd791ad3382
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-01-29 17:43:11 +0100

    module07/ex01 fin

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -1,8 +1,11 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T> void swap(T &x, T &y)
+template <typename T, typename B> void iter(T *arrayAdress, const int arrayLen, B func)
 {
-
+	for (int i = 0; i < arrayLen; ++i)
+	{
+		func(arrayAdress[i]);
+	}
 }
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <utility>
 #include "iter.hpp"
+
+template <typename T>
+void func(const T &i)
+{
+	std::cout << "element: " << i << "\n";	
+}
+
+class testClass{
+	private:
+		int _i;
+	public:
+		testClass(void) : _i(11) {return;};
+		int getI()const {return _i;};
+};
+std::ostream &operator<<(std::ostream &o, testClass const &other)
+{
+	o << "testClass: " << other.getI();
+	return o;
+}
 
 int main()
 {
+	std::string a[] = {"123", "555", "222", "111"};
+	const int b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	testClass c[3];
+	iter(c, 3, func<testClass>);
+	iter(a, 4, func<std::string>);
+	iter(b, 10, func<int>);
+
 	return 0;
 }
