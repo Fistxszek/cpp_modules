787cf19c20e2b2e3177d3d54b2f198aa320d253c
Fistxszek <kacper.orzechowski.04@gmail.com>
2026-01-29 13:56:16 +0100

    added missing char conversion

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -57,6 +57,17 @@ bool CheckType(std::string input, char *c, int *i, float *f, double *d)
 	if (CheckPseudoLiterals(input, f, d))
 		return false;
 
+	if (input.length() == 1 && (input[0] < '0' || input[0] > '9'))
+	{
+		*c = input[0];
+
+		if (*c <= 32 || *c >= 126)
+			return false;
+		*i = static_cast<int>(*c);
+		*f = static_cast<float>(*c);
+		*d = static_cast<double>(*c);
+		return true;
+	}
 	int charPos = input.find(".");
 	if (charPos == -1)
 	{
@@ -85,6 +96,8 @@ bool CheckType(std::string input, char *c, int *i, float *f, double *d)
 				*i = static_cast<int>(*d);
 			if (*d <= static_cast<double>(std::numeric_limits<float>::max()) && *d >= -static_cast<double>(std::numeric_limits<float>::max()))
 				*f = static_cast<float>(*d);
+			if (*d >= 0 && *d <= 127)
+				*c = static_cast<char>(*d);
 		}
 		else
 		{
@@ -98,6 +111,8 @@ bool CheckType(std::string input, char *c, int *i, float *f, double *d)
 			if (*f <= static_cast<float>(std::numeric_limits<int>::max()) && *f >= static_cast<float>(std::numeric_limits<int>::min()))
 				*i = static_cast<int>(*f);
 			*d = static_cast<double>(*f);
+			if (*f >= 0 && *f <= 127)
+				*c = static_cast<char>(*f);
 		}
 	}
 	return true;
@@ -105,7 +120,7 @@ bool CheckType(std::string input, char *c, int *i, float *f, double *d)
 
 void ScalarConverter::convert(std::string input)
 {
-	char c = 0;
+	char c = -1;
 	int i = 0;
 	float f = 0;
 	double d = 0;
@@ -125,7 +140,7 @@ void ScalarConverter::convert(std::string input)
 		return;
 	}
 
-	if (!c)
+	if (c < 0)
 		std::cout << "char: " << "impossible" << std::endl;	
 	else if (c > 32 && c < 126)
 		std::cout << "char: " << c << std::endl;	
@@ -140,7 +155,7 @@ void ScalarConverter::convert(std::string input)
 	double moduloVal = std::fmod(d, 2);
 	int	charPos = input.find(".");
 
-	if (charPos == -1 || (moduloVal == 0 && moduloVal == 1))
+	if (charPos == -1 || (moduloVal == 0 || moduloVal == 1))
 	{
 		if (f || f == d)
 			std::cout << "float: "<< f << ".0f" << std::endl;	
