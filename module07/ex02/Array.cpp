a1fd331d9f12d1c1d42b05cbcf07145076eb2efd
Kacper <kacper.orzechowski.04@gmail.com>
2026-01-30 16:41:52 +0100

    module07/ex02 working on

---
+++ module07/ex02/Array.cpp
@@ -0,0 +1,28 @@
+#include "Array.hpp"
+
+template<typename T>
+Array<T>::Array(void)
+{
+
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n)
+{
+
+}
+
+template<typename T>
+Array<T>::Array(const Array &other)
+{
+
+}
+
+template<typename T>
+Array<T> &Array<T>::operator=(const Array &other)
+{
+
+}
+
+template<typename T>
+Array::~Array(void);
---
+++ module07/ex02/Array.hpp
@@ -0,0 +1,17 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template <typename T>
+class Array
+{
+	private:
+		T _elements[];
+	public:
+		Array(void);
+		Array(unsigned int n);
+		Array(const Array &other);
+		Array &operator=(const Array &other);
+		~Array(void);
+};
+
+#endif
